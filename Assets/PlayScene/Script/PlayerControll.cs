22c00ea1a022c87bd0dd3c297557bd8a2dcd4f6d
エンドウ <117238687+endokazutoshi@users.noreply.github.com>
2024-11-14 10:58:55 +0900

    コントローラ操作（Lスティック)

--- Assets/PlayScene/Script/PlayerControll.cs
+++ Assets/PlayScene/Script/PlayerControll.cs
@@ -1,16 +1,35 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f; // 移動速度の調整
+    private int P1 = 1, P2 = 2;
+    public int PlayerNumber;
+    public float speed_H = 5f; // 移動速度の調整
+    public float speed_V = 3f;
 
     private void Update()
     {
-        // 水平方向の入力を取得
-        float moveInput = Input.GetAxis("Horizontal");
+        if (PlayerNumber == P1)
+        {
+            // 水平方向の入力を取得
+            float moveInput_H1 = Input.GetAxis("L_Stick_H_1P");
+            float moveInput_V1 = Input.GetAxis("L_Stick_V_1P");
 
-        // プレイヤーの移動処理
-        transform.Translate(Vector3.right * moveInput * speed * Time.deltaTime);
+            // プレイヤーの移動処理
+            transform.Translate(Vector3.right * moveInput_H1 * speed_H * Time.deltaTime);
+            transform.Translate(Vector3.down * moveInput_V1 * speed_V * Time.deltaTime);
+        }
+        else if (PlayerNumber == P2)
+        {
+            float moveInput_H2 = Input.GetAxis("L_Stick_H_2P");
+            float moveInput_V2 = Input.GetAxis("L_Stick_V_2P");
+
+            transform.Translate(Vector3.right * moveInput_H2 * speed_H * Time.deltaTime);
+            transform.Translate(Vector3.down * moveInput_V2 * speed_V * Time.deltaTime);
+        }
+        
+
     }
 }
 
