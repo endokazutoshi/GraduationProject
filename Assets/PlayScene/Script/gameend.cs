b6b8b4b2cd7a013f35af7b59c0533fec3285f765
kudousora <220341@jc-21.jp>
2024-11-28 16:15:55 +0900

    a

--- Assets/PlayScene/Script/gameend.cs
+++ Assets/PlayScene/Script/gameend.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class gameend : MonoBehaviour
 {
     // 他のオブジェクトと触れたときの処理
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("boxに触れました");
         // プレイヤータグが付いたオブジェクトに触れた場合
-        if (other.CompareTag("Player1"))
+        if (collider.CompareTag("Player1"))
         {
-            // 実行を終了する
             Debug.Log("プレイヤーがオブジェクトに触れました。終了します。");
+
+            // 実行を終了する
+            // エディタでも動作確認するためにエディタチェック
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
             Application.Quit();
+#endif
         }
     }
 }
