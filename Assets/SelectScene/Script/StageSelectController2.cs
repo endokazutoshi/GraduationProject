67b7a3d5211f391cb64d93a487965421e9620930
エンドウ <117238687+endokazutoshi@users.noreply.github.com>
2025-01-29 17:29:31 +0900

    StageSelectPlayer2フェードアウト（途中）

---
+++ Assets/SelectScene/Script/StageSelectController2.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class StageSelectController2 : MonoBehaviour
+{
+    public GameObject[] readySprite; // 準備OKの画像
+    public GameObject[] notReadySprite; // 準備中の画像
+    public GameObject[] targetText; // テキスト（GameObjectの表示切り替え）
+
+    private bool[] isPlayerReady = new bool[2]; // プレイヤーの準備状態
+
+    void Start()
+    {
+        UpdateReadyUI(0, false);
+        UpdateReadyUI(1, false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump_P1"))
+        {
+            isPlayerReady[0] = true;
+            UpdateReadyUI(0, true);
+        }
+        else if (Input.GetButtonDown("B_Button_1P"))
+        {
+            isPlayerReady[0] = false;
+            UpdateReadyUI(0, false);
+        }
+
+        if (Input.GetButtonDown("Jump_P2"))
+        {
+            isPlayerReady[1] = true;
+            UpdateReadyUI(1, true);
+        }
+        else if (Input.GetButtonDown("B_Button_2P"))
+        {
+            isPlayerReady[1] = false;
+            UpdateReadyUI(1, false);
+        }
+    }
+
+    private void UpdateReadyUI(int playerIndex, bool isReady)
+    {
+        if (readySprite[playerIndex] != null && notReadySprite[playerIndex] != null)
+        {
+            readySprite[playerIndex].SetActive(isReady);
+            notReadySprite[playerIndex].SetActive(!isReady);
+        }
+
+        if (targetText[playerIndex] != null)
+        {
+            targetText[playerIndex].SetActive(isReady);
+        }
+    }
+}
